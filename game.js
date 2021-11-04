1275791bce92d43907360d2619c8cef690c6a42b
Ade20150 <91483078+Ade20150@users.noreply.github.com>
2021-11-04 10:50:56 +0000

    general improvement

--- game.js
+++ game.js
@@ -141,73 +141,62 @@ function makeBees() {
     window.alert("Invalid number of bees");
     return;
   }
+
   //create bees
   let i = 1;
   while (i <= nbBees) {
     var num = i;
-    var bees = new Bee(num); //create object and its IMG element
+    var bee = new Bee(num); //create object and its IMG element
     bee.display(); //display the bee
     bees.push(bee); //add the bee object to the bees array
     i++;
   }
-  function moveBees() {
-    let speed = document.getElementById("speedBees").value; //move each bee to a random location
-    for (let i = 0; i < bees.length; i++) {
-      let dx = getRandomInt(2 * speed) - speed;
-      let dy = getRandomInt(2 * speed) - speed;
-      bees[i].move(dx, dy);
-      isHit(bees[i], bear); //we add this to count stings
-    }
+}
+function moveBees() {
+  let speed = document.getElementById("speedBees").value; //move each bee to a random location
+  for (let i = 0; i < bees.length; i++) {
+    let dx = getRandomInt(2 * speed) - speed;
+    let dy = getRandomInt(2 * speed) - speed;
+    bees[i].move(dx, dy);
+    isHit(bees[i], bear); //we add this to count stings
   }
-  function updateBees() {
-    // update loop for game //move the bees randomly
-    moveBees();
-    //use a fixed update period
-    let period = document.getElementById("periodTimer").value; //modify this to control refresh period
-    //update the timer for the next move
-    updateTimer = setTimeout("updateBees()", period);
-    if (Number(score) > 1000) {
-      //STOPS THE TIMER AND CLEARS IT
-      clearTimeout(updateTimer);
-      alert("Game over. Your score was " + score);
-    }
+}
+
+function updateBees() {
+  // update loop for game //move the bees randomly
+  moveBees();
+
+  //use a fixed update period
+  let period = document.getElementById("periodTimer").value; //modify this to control refresh period
+  //update the timer for the next move
+  updateTimer = setTimeout(updateBees(), period);
+}
+
+function isHit(defender, offender) {
+  if (overlap(defender, offender)) {
+    //check if the two image overlap
+    let score = hits.innerHTML;
+    score = Number(score) + 1; //increment the score
+    hits.innerHTML = score; //display the new score }
   }
-  function isHit(defender, offender) {
-    if (overlap(defender, offender)) {
-      //check if the two image overlap
-      let score = hits.innerHTML;
-      score = Number(score) + 1; //increment the score
-      hits.innerHTML = score; //display the new score }
-    }
-    function overlap(element1, element2) {
-      //consider the two rectangles wrapping the two elements //rectangle of the first element
-      left1 = element1.htmlElement.offsetLeft;
-      top1 = element1.htmlElement.offsetTop;
-      right1 =
-        element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
-      bottom1 =
-        element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight; //rectangle of the second element
-      left2 = element2.htmlElement.offsetLeft; //e2x
-      top2 = element2.htmlElement.offsetTop; //e2y
-      right2 =
-        element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
-      bottom2 =
-        element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
-      //calculate the intersection of the two rectangles
-      x_intersect = Math.max(
-        0,
-        Math.min(right1, right2) - Math.max(left1, left2)
-      );
-      y_intersect = Math.max(
-        0,
-        Math.min(bottom1, bottom2) - Math.max(top1, top2)
-      );
-      intersectArea = x_intersect * y_intersect;
-      //if intersection is nil no hit
-      if (intersectArea == 0 || isNaN(intersectArea)) {
-        return false;
-      }
-      return true;
-    }
+}
+function overlap(element1, element2) {
+  //consider the two rectangles wrapping the two elements //rectangle of the first element
+  left1 = element1.htmlElement.offsetLeft;
+  top1 = element1.htmlElement.offsetTop;
+  right1 = element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
+  bottom1 = element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight; //rectangle of the second element
+  left2 = element2.htmlElement.offsetLeft; //e2x
+  top2 = element2.htmlElement.offsetTop; //e2y
+  right2 = element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
+  bottom2 = element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
+  //calculate the intersection of the two rectangles
+  x_intersect = Math.max(0, Math.min(right1, right2) - Math.max(left1, left2));
+  y_intersect = Math.max(0, Math.min(bottom1, bottom2) - Math.max(top1, top2));
+  intersectArea = x_intersect * y_intersect;
+  //if intersection is nil no hit
+  if (intersectArea == 0 || isNaN(intersectArea)) {
+    return false;
   }
+  return true;
 }
